e15aeb00f47de0d0e49bf4a8ea73692cfdf42fbd
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2024-06-13 17:17:09 +0100

    feat: add hero section

--- app/Kids/page.tsx
+++ app/Kids/page.tsx
@@ -1,5 +1,11 @@
+import Hero from './Hero'
+
 const Kids = () => {
-    return <div></div>
+    return (
+        <div className="w-full h-full bg-white py-10">
+            <Hero />
+        </div>
+    )
 }
 
 export default Kids
